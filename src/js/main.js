eeb6bff8886f2014387adb425dae9a2f714fff71
Natalia-GP <nataliag86@yahoo.es>
2022-11-12 11:06:28 +0100

    html and querys

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
 'use strict';
+//QUERY SELECTORS
+const btn = document.querySelector('.js-btn');
+const inputSerch = document.querySelector('.js-input');
+const list = document.querySelector('.js-list');
 
-console.log('>> Ready :)');
