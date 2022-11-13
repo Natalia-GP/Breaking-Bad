a76b9537190097a50807dcc6da1cad9148563ab8
Natalia-GP <nataliag86@yahoo.es>
2022-11-13 12:54:24 +0100

    DOM Troubleshooting

--- src/js/main.js
+++ src/js/main.js
@@ -2,50 +2,68 @@
 console.log('añañañaña');
 //QUERYSELECTORS
 const btn = document.querySelector('.js-btn');
-const inputSerch = document.querySelector('.js-input');
+const inputSearch = document.querySelector('.js-input');
 const listCharacter = document.querySelector('.js-list');
-const favourCharacter = document.querySelector('.js-favourite');
+const favCharacter = document.querySelector('.js-favorite');
 
 let allCharacter = []; //array todo los pj
-let serchList = [];
-let favCharacter = []; //array personajs favoritos
+let searchList = [];
+let favList = []; //array personajs favoritos
 //VARIABLES GLOBALES-> VARIABLES CON DATOS (PERSONAJES Y FAV)
 
 //FUNCIONES
 
-//fech del API
+//fetch del API
 fetch('https://breakingbadapi.com/api/characters')
   .then((response) => response.json())
   .then((data) => {
     allCharacter = data;
-    renderAllCharacters(data, allCharacter);
+    renderAllCharacters(allCharacter, listCharacter);
   });
-//bucle para el array de datos
-function renderAllCharacters(allCharacter) {
-  for (const character of allCharacter) {
-    renderCharacters(character);
+//bucle para pintar array de datos
+function renderAllCharacters(dataChar, htmlList) {
+  htmlList.innerHTML = '';
+  for (const character of dataChar) {
+    renderCharacters(character, htmlList);
   }
 }
 //función para pintar cada personaje con DOM avanzado
-function renderCharacters(characterData) {
+function renderCharacters(characterData, htmlList) {
   const liElement = document.createElement('li');
   const articleElement = document.createElement('article');
   const imgElement = document.createElement('img');
   const nameElement = document.createElement('h3');
   const statusElement = document.createElement('p');
-  liElement.setAttribute('data-id', `${characterData.char_id}`);
-  articleElement.setAttribute('class', 'card');
+  articleElement.setAttribute('id', `${characterData.char_id}`);
   imgElement.setAttribute('src', characterData.img);
   imgElement.setAttribute('alt', `${characterData.name}`);
   const nameText = document.createTextNode(characterData.name);
   const nameStatus = document.createTextNode(characterData.status);
+  articleElement.setAttribute('class', 'section__list__article');
+  imgElement.setAttribute('class', 'section__list__article--img');
   liElement.appendChild(articleElement);
   articleElement.appendChild(imgElement);
   articleElement.appendChild(nameElement);
   articleElement.appendChild(statusElement);
-  listCharacter.appendChild(liElement);
+  nameElement.appendChild(nameText);
+  statusElement.appendChild(nameStatus);
+  htmlList.appendChild(liElement);
+  
+}
+//función para filtrar
+function handleClick(event) {
+  event.preventDefault();
+  const userSearch = inputSearch.value;
+  fetch(`https://breakingbadapi.com/api/characters?name=${userSearch}`)
+    .then((response) => response.json())
+    .then((data) => {
+      searchList = data;
+      renderAllCharacters(searchList, listCharacter);
+    });
+
 }
 //EVENTOS
-/* btn.addEventListener('click', handleClick); */
+btn.addEventListener('click', handleClick);
+
 
 //CÓDIGO QUE SE EJECUTA AL CARGAR LA PÁGINA
