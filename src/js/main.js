63fee251e4047eb51b8ff56ae07bf649bd753c13
Natalia-GP <nataliag86@yahoo.es>
2022-11-14 09:18:22 +0100

    js-favourites and first css

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ console.log('añañañaña');
 const btn = document.querySelector('.js-btn');
 const inputSearch = document.querySelector('.js-input');
 const listCharacter = document.querySelector('.js-list');
-const favCharacter = document.querySelector('.js-favorite');
+const favCharacter = document.querySelector('.js-favourite');
 
 let allCharacter = []; //array todo los pj
 let searchList = [];
@@ -48,7 +48,7 @@ function renderCharacters(characterData, htmlList) {
   nameElement.appendChild(nameText);
   statusElement.appendChild(nameStatus);
   htmlList.appendChild(liElement);
-  
+  addEvent();
 }
 //función para filtrar
 function handleClick(event) {
@@ -65,5 +65,32 @@ function handleClick(event) {
 //EVENTOS
 btn.addEventListener('click', handleClick);
 
+//FUNCIÓN AÑADIR FAV
 
-//CÓDIGO QUE SE EJECUTA AL CARGAR LA PÁGINA
+function handleFavourites (event) {
+  event.preventDefault();
+  const target = event.currentTarget;
+  if (target.classList.contains('favourite')){
+    target.classList.remove('favourite');
+    const position =searchArray(parseInt(target.id));
+    favList.splice(position, 1);
+  } else{
+    target.classList.add('favourite');
+    favList.push(  //agrega
+      allCharacter.find((eachCharacter) => eachCharacter.char_id === parseInt(target.id))
+    );
+  }
+  localStorage.setItem('favChar', JSON.stringify(favList)); //guardar los datos y convertir el array en cadena
+  renderAllCharacters(favList, favCharacter); //pintamos
+}
+//EVENTO ESCUCHA CADA PJ
+function addEvent() {
+  const characters = document.querySelectorAll('.section__list__article');
+  for (const eachCharacter of characters) {
+    eachCharacter.addEventListener('click', handleFavourites);
+  }
+}
+
+function searchArray(target) {
+  return favList.findIndex((each) => each.char_id === target);
+}
