74c70394c23c5c8f1dc796d045781acbe795f51e
Natalia-GP <nataliag86@yahoo.es>
2022-11-15 09:14:06 +0100

    js favourites, html changes,css fav

--- src/js/main.js
+++ src/js/main.js
@@ -13,13 +13,19 @@ let favList = []; //array personajs favoritos
 
 //FUNCIONES
 
-//fetch del API
+//fetch del API carga de página
 fetch('https://breakingbadapi.com/api/characters')
   .then((response) => response.json())
   .then((data) => {
     allCharacter = data;
     renderAllCharacters(allCharacter, listCharacter);
+    addEvent();
+    if (localStorage.getItem('favChar')) {  //saque los fav del LSa
+      favList = JSON.parse(localStorage.getItem('favChar')); //parse=cambiar de texto a objeto
+      renderAllCharacters(favList, favCharacter);  //pinta
+    }
   });
+
 //bucle para pintar array de datos
 function renderAllCharacters(dataChar, htmlList) {
   htmlList.innerHTML = '';
@@ -29,12 +35,12 @@ function renderAllCharacters(dataChar, htmlList) {
 }
 //función para pintar cada personaje con DOM avanzado
 function renderCharacters(characterData, htmlList) {
-  const liElement = document.createElement('li');
+  const liElement = document.createElement('li');  //crea
   const articleElement = document.createElement('article');
   const imgElement = document.createElement('img');
   const nameElement = document.createElement('h3');
   const statusElement = document.createElement('p');
-  articleElement.setAttribute('id', `${characterData.char_id}`);
+  articleElement.setAttribute('id', `${characterData.char_id}`);  //añade
   imgElement.setAttribute('src', characterData.img);
   imgElement.setAttribute('alt', `${characterData.name}`);
   const nameText = document.createTextNode(characterData.name);
@@ -42,13 +48,13 @@ function renderCharacters(characterData, htmlList) {
   articleElement.setAttribute('class', 'section__list__article');
   imgElement.setAttribute('class', 'section__list__article--img');
   liElement.appendChild(articleElement);
-  articleElement.appendChild(imgElement);
+  articleElement.appendChild(imgElement); //mete
   articleElement.appendChild(nameElement);
   articleElement.appendChild(statusElement);
   nameElement.appendChild(nameText);
   statusElement.appendChild(nameStatus);
   htmlList.appendChild(liElement);
-  addEvent();
+  addEvent(); //escucha pj
 }
 //función para filtrar
 function handleClick(event) {
@@ -70,18 +76,24 @@ btn.addEventListener('click', handleClick);
 function handleFavourites (event) {
   event.preventDefault();
   const target = event.currentTarget;
-  if (target.classList.contains('favourite')){
-    target.classList.remove('favourite');
-    const position =searchArray(parseInt(target.id));
-    favList.splice(position, 1);
-  } else{
+  if (searchArray(parseInt(target.id)) === -1) {
     target.classList.add('favourite');
-    favList.push(  //agrega
-      allCharacter.find((eachCharacter) => eachCharacter.char_id === parseInt(target.id))
+    favList.push(
+      allCharacter.find(
+        (eachCharacter) => eachCharacter.char_id === parseInt(target.id)
+      )
     );
+  } else {
+    target.classList.remove('favourite');
+    const position = searchArray(parseInt(target.id));
+    favList.splice(position, 1);
   }
-  localStorage.setItem('favChar', JSON.stringify(favList)); //guardar los datos y convertir el array en cadena
+  localStorage.setItem('favChar', JSON.stringify(favList)); //guardar los datos del array en el LocalSoreage y convertir el array en cadena
   renderAllCharacters(favList, favCharacter); //pintamos
+}
+//buscar en un array
+function searchArray(target) {
+  return favList.findIndex((each) => each.char_id === target);
 }
 //EVENTO ESCUCHA CADA PJ
 function addEvent() {
@@ -89,8 +101,4 @@ function addEvent() {
   for (const eachCharacter of characters) {
     eachCharacter.addEventListener('click', handleFavourites);
   }
-}
-
-function searchArray(target) {
-  return favList.findIndex((each) => each.char_id === target);
 }
