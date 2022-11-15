5fa6197c004162f66f8c018f97fb41c6501a747e
Natalia-GP <nataliag86@yahoo.es>
2022-11-15 11:47:09 +0100

    mark character and fix CSS problems

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,10 @@ const btn = document.querySelector('.js-btn');
 const inputSearch = document.querySelector('.js-input');
 const listCharacter = document.querySelector('.js-list');
 const favCharacter = document.querySelector('.js-favourite');
-
+//VARIABLES GLOBALES-> VARIABLES CON DATOS (PERSONAJES Y FAV)
 let allCharacter = []; //array todo los pj
 let searchList = [];
 let favList = []; //array personajs favoritos
-//VARIABLES GLOBALES-> VARIABLES CON DATOS (PERSONAJES Y FAV)
 
 //FUNCIONES
 
@@ -20,27 +19,31 @@ fetch('https://breakingbadapi.com/api/characters')
     allCharacter = data;
     renderAllCharacters(allCharacter, listCharacter);
     addEvent();
-    if (localStorage.getItem('favChar')) {  //saque los fav del LSa
+    if (localStorage.getItem('favChar')) {
+      //saque los fav del LSa
       favList = JSON.parse(localStorage.getItem('favChar')); //parse=cambiar de texto a objeto
-      renderAllCharacters(favList, favCharacter);  //pinta
+      renderAllCharacters(favList, favCharacter); //pinta
     }
   });
 
-//bucle para pintar array de datos
+//bucle para pintar todos los pj
 function renderAllCharacters(dataChar, htmlList) {
-  htmlList.innerHTML = '';
+  htmlList.innerHTML = ''; //mi hoja en blanco
   for (const character of dataChar) {
+    //bucle, recorrerá todo el obj
     renderCharacters(character, htmlList);
   }
 }
+
+
 //función para pintar cada personaje con DOM avanzado
 function renderCharacters(characterData, htmlList) {
-  const liElement = document.createElement('li');  //crea
+  const liElement = document.createElement('li'); //crea
   const articleElement = document.createElement('article');
   const imgElement = document.createElement('img');
   const nameElement = document.createElement('h3');
   const statusElement = document.createElement('p');
-  articleElement.setAttribute('id', `${characterData.char_id}`);  //añade
+  articleElement.setAttribute('id', `${characterData.char_id}`); //añade
   imgElement.setAttribute('src', characterData.img);
   imgElement.setAttribute('alt', `${characterData.name}`);
   const nameText = document.createTextNode(characterData.name);
@@ -54,7 +57,7 @@ function renderCharacters(characterData, htmlList) {
   nameElement.appendChild(nameText);
   statusElement.appendChild(nameStatus);
   htmlList.appendChild(liElement);
-  addEvent(); //escucha pj
+  addEvent(); //escucha pj para asignar después los eventos
 }
 //función para filtrar
 function handleClick(event) {
@@ -66,18 +69,17 @@ function handleClick(event) {
       searchList = data;
       renderAllCharacters(searchList, listCharacter);
     });
-
 }
 //EVENTOS
 btn.addEventListener('click', handleClick);
 
-//FUNCIÓN AÑADIR FAV
+//FUNCIÓN AÑADIR FAV y pintarlos
 
-function handleFavourites (event) {
+function handleFavourites(event) {
   event.preventDefault();
   const target = event.currentTarget;
-  if (searchArray(parseInt(target.id)) === -1) {
-    target.classList.add('favourite');
+  if (searchArray(parseInt(target.id)) === -1) { //convierte
+    target.classList.add('favourite');//añado la clase favourite
     favList.push(
       allCharacter.find(
         (eachCharacter) => eachCharacter.char_id === parseInt(target.id)
@@ -95,10 +97,13 @@ function handleFavourites (event) {
 function searchArray(target) {
   return favList.findIndex((each) => each.char_id === target);
 }
-//EVENTO ESCUCHA CADA PJ
+
+//EVENTO ESCUCHA CADA PJ y fv
 function addEvent() {
-  const characters = document.querySelectorAll('.section__list__article');
-  for (const eachCharacter of characters) {
-    eachCharacter.addEventListener('click', handleFavourites);
+  const characters = document.querySelectorAll('.section__list__article');//array con todos los elem
+  for (const eachCharacter of characters) {  //creamos el bucle
+    if (!eachCharacter.classList.contains('aside__favourite__li--article')) {
+      eachCharacter.addEventListener('click', handleFavourites);
+    }
   }
 }
